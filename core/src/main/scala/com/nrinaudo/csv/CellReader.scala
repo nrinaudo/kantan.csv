eaf0d660a83ec9484a87fdd9502c800eb5852570
nrinaudo <nicolas@nrinaudo.com>
2015-09-25 19:57:15 +0200

    Document CellReader.
    I've also added a map function which might be useful in the future. Since a CellReader is essentially a Function1[String, A], we could add the whole set of monadic functions, but this is probably overkill and I don't really see the point.

--- core/src/main/scala/com/nrinaudo/csv/CellReader.scala
+++ core/src/main/scala/com/nrinaudo/csv/CellReader.scala
@@ -2,7 +2,9 @@ package com.nrinaudo.csv
 
 import simulacrum.typeclass
 
+/** Provides default implementations and construction methods for {{{CellReader}}}. */
 object CellReader {
+  /** Creates a new instance of {{{CellReader}}} that uses the specified function to parse data. */
   def apply[A](f: String => A): CellReader[A] = new CellReader[A] {
     override def read(a: String) = f(a)
   }
@@ -17,6 +19,20 @@ object CellReader {
   implicit val bool  : CellReader[Boolean] = apply(_.toBoolean)
 }
 
+/** Typeclass used to reader the content of a single CSV cell.
+  *
+  * Standard types are already provided for in the companion object.
+  *
+  * See also the companion class, {{{CellWriter}}}, as well as {{{RowReader}}}, used to read wholes rows.
+  */
 @typeclass trait CellReader[A] {
+  /** Turns the content of a CSV cell into an {{{A}}}. */
   def read(a: String): A
+
+  /** Creates a new {{{CellReader}}} that applies the specified function to the result of {{{read}}}.
+    *
+    * This can be useful if you just need a {{{CellReader}}} implementation that specialises an existing type - to add
+    * a scalaz tag, say.
+    */
+  def map[B](f: A => B): CellReader[B] = CellReader(str => f(read(str)))
 }
