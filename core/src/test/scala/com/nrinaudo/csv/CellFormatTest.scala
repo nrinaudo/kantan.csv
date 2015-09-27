3470266abb35ef3bfbbcc7c6026908822b419d64
nrinaudo <nicolas@nrinaudo.com>
2015-09-27 15:21:15 +0200

    Add tests for CellReader and CellWriter.

---
+++ core/src/test/scala/com/nrinaudo/csv/CellFormatTest.scala
@@ -0,0 +1,20 @@
+package com.nrinaudo.csv
+
+import org.scalacheck.Arbitrary
+import org.scalatest.FunSuite
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+abstract class CellFormatTest[A: CellFormat: Arbitrary] extends FunSuite with GeneratorDrivenPropertyChecks {
+  test("Writing then reading data should leave it unchanged") {
+    forAll { a: A => assert(CellReader[A].read(CellWriter[A].write(a)) == a)  }
+  }
+}
+
+class StringFormatTest extends CellFormatTest[String]
+class IntFormatTest extends CellFormatTest[Int]
+class DoubleFormatTest extends CellFormatTest[Double]
+class LongFormatTest extends CellFormatTest[Long]
+class FloatFormatTest extends CellFormatTest[Float]
+class ShortFormatTest extends CellFormatTest[Short]
+class ByteFormatTest extends CellFormatTest[Byte]
+class BooleanFormatTest extends CellFormatTest[Boolean]
--- scalacheck/src/main/scala/com/nrinaudo/csv/scalacheck/package.scala
+++ scalacheck/src/main/scala/com/nrinaudo/csv/scalacheck/package.scala
@@ -6,5 +6,4 @@ package object scalacheck {
   // CSV generators for property based testing.
   def cell: Gen[String] = Gen.nonEmptyListOf(Gen.choose(0x20.toChar, 0x7e.toChar)).map(_.mkString)
   def csv: Gen[List[List[String]]] = Gen.nonEmptyListOf(Gen.nonEmptyListOf(cell))
-
 }
