ff7f967ba3730f3764639b9246cd56fa09ffccb8
nrinaudo <nicolas@nrinaudo.com>
2015-09-30 11:06:48 +0200

    Update CSV serialisation to use string interpolation rather than concatenation.

--- core/src/main/scala/com/nrinaudo/csv/CsvWriter.scala
+++ core/src/main/scala/com/nrinaudo/csv/CsvWriter.scala
@@ -6,8 +6,8 @@ class CsvWriter[A] private[csv] (private val out: PrintWriter, val sep: Char, pr
   extends Closeable {
 
   private def escape(str: String): String =
-    if(str.contains("\""))                                                 "\"" + str.replaceAll("\"", "\"\"") + "\""
-    else if(str.contains(sep) || str.contains("\n") || str.contains("\r")) "\"" + str + "\""
+    if(str.contains("\""))                                                 s""""${str.replaceAll("\"", "\"\"")}""""
+    else if(str.contains(sep) || str.contains("\n") || str.contains("\r")) s""""$str""""
     else                                                                   str
 
   private def write(ss: Seq[String]): Unit = ss.map(escape) match {
