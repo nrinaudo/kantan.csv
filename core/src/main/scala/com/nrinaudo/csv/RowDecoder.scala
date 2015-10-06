13d5cdfcc3936900e7c29619cb24b5729263ec71
nrinaudo <nicolas@nrinaudo.com>
2015-10-06 22:39:53 +0200

    Document RowDecoder (WIP).

--- core/src/main/scala/com/nrinaudo/csv/RowDecoder.scala
+++ core/src/main/scala/com/nrinaudo/csv/RowDecoder.scala
@@ -4,27 +4,63 @@ import simulacrum.{noop, typeclass}
 
 import scala.collection.generic.CanBuildFrom
 
-/** Type class for decoding the content of a CSV row.
+/** Decodes CSV rows into usable types.
   *
-  * Default implementations are provided in the companion object.
+  * When implementing a custom `RowDecoder` instance, the correct way of parsing each cell is by retrieving a
+  * `CellDecoder` for the correct type and delegating the job to it, combining the result through `map` and `flatMap`
+  * (or for-comprehensions). For example:
+  * {{{
+  *   case class Point2D(x: Int, y: Int)
+  *
+  *   implicit val p2dDecoder = RowDecoder { ss =>
+  *     for {
+  *       x <- CellDecoder[Int].decode(ss, 0)
+  *       y <- CellDecoder[Int].decode(ss, 1)
+  *     } yield new Point2D(x, y)
+  *   }
+  * }}}
+  *
+  * See the [[RowDecoder$ companion object]] for default implementations and construction methods.
   */
 @typeclass trait RowDecoder[A] { self =>
+  /** Turns the content of a row into `A`. */
   @noop def decode(row: Seq[String]): DecodeResult[A]
+
+  /** Turns a `RowDecoder[A]` into a `RowDecoder[B]`.
+    *
+    * This allows developers to adapt existing instances of `RowDecoder` rather than write one from scratch.
+    */
   @noop def map[B](f: A => B): RowDecoder[B] = RowDecoder(ss => decode(ss).map(f))
 }
 
+/** Defines convenience methods for creating and retrieving instances of `RowDecoder`.
+  *
+  * Implicit default implementations of standard types are also declared here, always bringing them in scope with a low
+  * priority.
+  *
+  * These default implementations can also be useful when writing more complex instances: if you need to write a
+  * `RowDecoder[B]` and have both a `RowDecoder[A]` and a `A => B`, you need just use [[RowDecoder.map]] to create
+  * your implementation.
+  */
 object RowDecoder {
+  /** Creates a new instance of [[RowDecoder]] that uses the specified function to parse data. */
   def apply[A](f: Seq[String] => DecodeResult[A]): RowDecoder[A] = new RowDecoder[A] {
     override def decode(row: Seq[String]) = f(row)
   }
 
+  /** Parses CSV rows as sequences of strings. */
   implicit val stringSeq: RowDecoder[Seq[String]] = RowDecoder(ss => DecodeResult.success(ss))
 
+  /** Parses a CSV row into an `Either[A, B]`.
+    *
+    * This is done by first attempting to parse the row as an `A`. If that fails, we'll try parsing it as a `B`. If that
+    * fails as well, [[DecodeResult.DecodeFailure]] will be returned.
+    */
   implicit def either[A: RowDecoder, B: RowDecoder]: RowDecoder[Either[A, B]] = RowDecoder { ss =>
     RowDecoder[A].decode(ss).map(a => Left(a): Either[A, B]).orElse(RowDecoder[B].decode(ss).map(b => Right(b): Either[A, B]))
   }
 
-  /** Generic `RowDecoder` for collections. */
+  /** Parses a CSV row into a collection of `A`. */
   implicit def collection[A: CellDecoder, M[X]](implicit cbf: CanBuildFrom[Nothing, A, M[A]]): RowDecoder[M[A]] =
     RowDecoder(ss => ss.foldLeft(DecodeResult.success(cbf.apply())) { (racc, s) => for {
       acc <- racc
