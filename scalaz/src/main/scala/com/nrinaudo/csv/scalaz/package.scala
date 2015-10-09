529338b75ab39b692a4f6f57c9966c768da454db
nrinaudo <nicolas@nrinaudo.com>
2015-10-09 10:39:26 +0200

    Fix some "remote" documentation links.

--- scalaz/src/main/scala/com/nrinaudo/csv/scalaz/package.scala
+++ scalaz/src/main/scala/com/nrinaudo/csv/scalaz/package.scala
@@ -10,14 +10,14 @@ import _root_.scalaz.syntax.maybe._
 package object scalaz {
   // - DecodeResult ----------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
-  /** [[Monad]] instance for [[DecodeResult]]. */
+  /** `Monad` instance for [[DecodeResult]]. */
   implicit val decodeResultInstances: Monad[DecodeResult] = new Monad[DecodeResult] {
     override def bind[A, B](fa: DecodeResult[A])(f: A => DecodeResult[B]) = fa.flatMap(f)
     override def map[A, B](fa: DecodeResult[A])(f: A => B) = fa.map(f)
     override def point[A](x: => A) = DecodeResult(x)
   }
 
-  /** [[Equal]] instance for [[DecodeResult]]. */
+  /** `Equal` instance for [[DecodeResult]]. */
   implicit def decodeResultEqual[A: Equal]: Equal[DecodeResult[A]] = new Equal[DecodeResult[A]] {
     override def equal(a1: DecodeResult[A], a2: DecodeResult[A]) = (a1, a2) match {
       case (DecodeResult.Success(a), DecodeResult.Success(b))                   => Equal[A].equal(a, b)
@@ -30,14 +30,14 @@ package object scalaz {
 
   // - CellCodec -------------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
-  /** [[Monad]] instance for [[CellDecoder]]. */
+  /** `Monad` instance for [[CellDecoder]]. */
   implicit val cellDecoder: Monad[CellDecoder] = new Monad[CellDecoder] {
     override def map[A, B](fa: CellDecoder[A])(f: A => B) = fa.map(f)
     override def point[A](a: => A) = CellDecoder(_ => DecodeResult(a))
     override def bind[A, B](fa: CellDecoder[A])(f: A => CellDecoder[B]) = fa.flatMap(f)
   }
 
-  /** [[Contravariant]] instance for [[CellEncoder]]. */
+  /** `Contravariant` instance for [[CellEncoder]]. */
   implicit val cellEncoder: Contravariant[CellEncoder] = new Contravariant[CellEncoder] {
     override def contramap[A, B](r: CellEncoder[A])(f: B => A) = r.contramap(f)
   }
@@ -46,14 +46,14 @@ package object scalaz {
 
   // - RowCodec --------------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
-  /** [[Monad]] instance for [[RowDecoder]]. */
+  /** `Monad` instance for [[RowDecoder]]. */
   implicit val rowDecoder: Monad[RowDecoder] = new Monad[RowDecoder] {
     override def map[A, B](fa: RowDecoder[A])(f: A => B) = fa.map(f)
     override def point[A](a: => A) = RowDecoder(_ => DecodeResult(a))
     override def bind[A, B](fa: RowDecoder[A])(f: A => RowDecoder[B]) = fa.flatMap(f)
   }
 
-  /** [[Contravariant]] instance for [[CellEncoder]]. */
+  /** `Contravariant` instance for [[CellEncoder]]. */
   implicit val rowEncoder: Contravariant[RowEncoder] = new Contravariant[RowEncoder] {
     override def contramap[A, B](r: RowEncoder[A])(f: B => A) = r.contramap(f)
   }
@@ -62,12 +62,12 @@ package object scalaz {
 
   // - CSV input / output ----------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
-  /** [[Contravariant]] instance for [[CsvInput]]. */
+  /** `Contravariant` instance for [[CsvInput]]. */
   implicit val csvInput: Contravariant[CsvInput] = new Contravariant[CsvInput] {
     override def contramap[A, B](r: CsvInput[A])(f: B => A) = r.contramap(f)
   }
 
-  /** [[Contravariant]] instance for [[CsvOutput]]. */
+  /** `Contravariant` instance for [[CsvOutput]]. */
   implicit val csvOutput: Contravariant[CsvOutput] = new Contravariant[CsvOutput] {
     override def contramap[A, B](r: CsvOutput[A])(f: B => A) = r.contramap(f)
   }
@@ -75,13 +75,13 @@ package object scalaz {
 
   // - Maybe -----------------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
-  /** [[CellDecoder]] instance for [[Maybe]]. */
+  /** [[CellDecoder]] instance for `Maybe`. */
   implicit def maybeDecoder[A: CellDecoder]: CellDecoder[Maybe[A]] = CellDecoder { s =>
     if(s.isEmpty) DecodeResult.success(empty)
     else          CellDecoder[A].decode(s).map(just)
   }
 
-  /** [[CellEncoder]] instance for [[Maybe]]. */
+  /** [[CellEncoder]] instance for `Maybe`. */
   implicit def maybeEncoder[A: CellEncoder]: CellEncoder[Maybe[A]] =
     CellEncoder(ma => ma.map(CellEncoder[A].encode).getOrElse(""))
 
@@ -89,25 +89,25 @@ package object scalaz {
 
   // - \/ --------------------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
-  /** [[CellDecoder]] instance for [[\/]]. */
+  /** [[CellDecoder]] instance for `\/`. */
   implicit def eitherCellDecoder[A: CellDecoder, B: CellDecoder]: CellDecoder[A \/ B] =
     CellDecoder { s => CellDecoder[A].decode(s).map(_.left[B])
       .orElse(CellDecoder[B].decode(s).map(_.right[A]))
     }
 
-  /** [[CellEncoder]] instance for [[\/]]. */
+  /** [[CellEncoder]] instance for `\/`. */
   implicit def eitherCellEncoder[A: CellEncoder, B: CellEncoder]: CellEncoder[A \/ B] = CellEncoder(eab => eab match {
     case -\/(a)  => a.asCsvCell
     case \/-(b)  => b.asCsvCell
   })
 
-  /** [[RowDecoder]] instance for [[\/]]. */
+  /** [[RowDecoder]] instance for `\/`. */
   implicit def eitherRowDecoder[A: RowDecoder, B: RowDecoder]: RowDecoder[A \/ B] =
     RowDecoder { s => RowDecoder[A].decode(s).map(_.left[B])
       .orElse(RowDecoder[B].decode(s).map(_.right[A]))
     }
 
-  /** [[RowEncoder]] instance for [[\/]]. */
+  /** [[RowEncoder]] instance for `\/`. */
   implicit def eitherRowEncoder[A: RowEncoder, B: RowEncoder]: RowEncoder[A \/ B] = RowEncoder(eab => eab match {
     case -\/(a)  => a.asCsvRow
     case \/-(b)  => b.asCsvRow
