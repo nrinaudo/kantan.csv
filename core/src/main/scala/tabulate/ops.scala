a794dd5cae92192cd64a0a8b7ee5b72d240443d6
nrinaudo <nicolas@nrinaudo.com>
2015-11-14 15:57:04 +0100

    Add String.parseCsvCell and Seq[String].parseCsvRow operators.

--- core/src/main/scala/tabulate/ops.scala
+++ core/src/main/scala/tabulate/ops.scala
@@ -3,4 +3,12 @@ package tabulate
 object ops extends CsvInput.ToCsvInputOps
                    with CsvOutput.ToCsvOutputOps
                    with RowEncoder.ToRowEncoderOps
-                   with CellEncoder.ToCellEncoderOps
+                   with CellEncoder.ToCellEncoderOps {
+  implicit class CellDecoderOps(val str: String) extends AnyVal {
+    def parseCsvCell[A: CellDecoder]: DecodeResult[A] = CellDecoder[A].decode(str)
+  }
+
+  implicit class RowDecoderOps(val row: Seq[String]) extends AnyVal {
+    def parseCsvRow[A: RowDecoder]: DecodeResult[A] = RowDecoder[A].decode(row)
+  }
+}
