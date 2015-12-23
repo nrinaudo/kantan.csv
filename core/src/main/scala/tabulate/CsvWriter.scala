a8be9c44ffee549f47f9a4a7fbd878007b7724e3
nrinaudo <nicolas@nrinaudo.com>
2015-12-23 15:16:39 +0100

    Add explicit CswWriter tests.
    CsvWriter is also much faster, with fewer traversals of each cell in a row and a slightly smaller memory footprint.

--- core/src/main/scala/tabulate/CsvWriter.scala
+++ core/src/main/scala/tabulate/CsvWriter.scala
@@ -1,22 +1,51 @@
 package tabulate
 
-import java.io.{Writer, Closeable, PrintWriter}
+import java.io.{Closeable, Writer}
+
+import scala.annotation.tailrec
 
 class CsvWriter[A] private[tabulate] (private val out: Writer, val sep: Char, private val format: A => Seq[String])
   extends Closeable {
 
-  private def escape(str: String): String =
-    if(str.contains("\""))                                                 s""""${str.replaceAll("\"", "\"\"")}""""
-    else if(str.contains(sep) || str.contains("\n") || str.contains("\r")) s""""$str""""
-    else                                                                   str
+  private def safeWrite(str: String): Unit = {
+    @tailrec
+    def escape(mark: Int, i: Int): Unit =
+      if(i >= str.length) {
+        if(mark != i) out.write(str, mark, i - mark)
+      }
+      else if(str.charAt(i) == '"') {
+        out.write(str, mark, i - mark + 1)
+        out.write('"')
+        escape(i + 1, i + 1)
+      }
+      else escape(mark, i + 1)
+
+    @tailrec
+    def escapeIndex(index: Int): Int =
+      if(index >= str.length) -1
+      else {
+        val c = str.charAt(index)
+        if(c == '"' || c == sep || c == '\n' || c == '\r') index
+        else escapeIndex(index + 1)
+      }
+
+    val index = escapeIndex(0)
+
+    if(index == -1) out.write(str)
+    else {
+      out.write('"')
+      out.write(str, 0, index)
+      escape(index, index)
+      out.write('"')
+    }
+  }
 
   private def write(ss: Seq[String]): Unit = {
-    val fs = ss.map(escape)
-    fs.headOption.foreach { h =>
-      out.write(h)
-      fs.tail.foreach { a =>
+    ss.headOption.foreach { h =>
+      safeWrite(h)
+      ss.tail.foreach { a =>
         out.write(sep.toInt)
-        out.write(a)
+        safeWrite(a)
       }
       out.write("\r\n") // According to the RFC, \n alone is not valid.
     }
