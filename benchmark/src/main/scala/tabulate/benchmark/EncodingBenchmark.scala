78ce4600d7b371410fc6266fa21e15b64db206f2
nrinaudo <nicolas@nrinaudo.com>
2015-12-23 17:50:10 +0100

    Add opencsv to the list of libraries benchmarked for output.

--- benchmark/src/main/scala/tabulate/benchmark/EncodingBenchmark.scala
+++ benchmark/src/main/scala/tabulate/benchmark/EncodingBenchmark.scala
@@ -1,6 +1,6 @@
 package tabulate.benchmark
 
-import java.io.StringWriter
+import java.io.{StringWriter, Writer}
 import java.util.concurrent.TimeUnit
 
 import org.openjdk.jmh.annotations._
@@ -10,13 +10,25 @@ import tabulate.ops._
 @BenchmarkMode(Array(Mode.AverageTime))
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 class EncodingBenchmark {
+  def write[A](f: Array[String] => Unit): Unit =
+    rawData.foreach { entry => f(Array(entry._1.toString, entry._2.toString, entry._3.toString, entry._4.toString)) }
+
   @Benchmark
-  def tabulate() = new StringWriter().writeCsv(rawData, ',').toString
+  def tabulate() = new StringWriter().writeCsv(rawData, ',').close()
 
   @Benchmark
   def productCollections() = {
     val out = new StringWriter()
-    com.github.marklister.collections.io.Utils.CsvOutput(rawData).writeCsv(out)
-    out.toString
+    com.github.marklister.collections.io.Utils.CsvOutput(rawData).writeCsv(out, ",")
+    out.close()
+  }
+
+  @Benchmark
+  def opencsv() = {
+    val out = new StringWriter()
+    val writer = new com.opencsv.CSVWriter(out, ',')
+    write { a => writer.writeNext(a) }
+    writer.close()
+    out.close()
   }
 }
