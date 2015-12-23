b6c41e301d61dcde989447d652605991decb4098
nrinaudo <nicolas@nrinaudo.com>
2015-12-23 09:30:41 +0100

    Re-enable test that was commented out by mistake.

--- tests/src/test/scala/tabulate/NonRegressionTests.scala
+++ tests/src/test/scala/tabulate/NonRegressionTests.scala
@@ -5,7 +5,7 @@ import ops._
 
 class NonRegressionTests extends FunSuite {
   test("cell with whitespace") {
-    //assert("abc, \n".asUnsafeCsvRows[List[String]](',', false).toList == List(List("abc", " ")))
+    assert("abc, \n".asUnsafeCsvRows[List[String]](',', false).toList == List(List("abc", " ")))
   }
 
   test("CRLF in escaped") {
