6a99725011503e3e9a4b461c32bc09cd9e0d199d
nrinaudo <nicolas@nrinaudo.com>
2015-12-27 22:03:48 +0100

    Add an implicit WriterEngine to TraversableOps.

--- core/src/main/scala/tabulate/ops.scala
+++ core/src/main/scala/tabulate/ops.scala
@@ -2,6 +2,8 @@ package tabulate
 
 import java.io.StringWriter
 
+import tabulate.engine.WriterEngine
+
 object ops extends CsvInput.ToCsvInputOps
                    with CsvOutput.ToCsvOutputOps
                    with RowEncoder.ToRowEncoderOps
@@ -14,7 +16,7 @@ object ops extends CsvInput.ToCsvInputOps
     def parseCsvRow[A](implicit da: RowDecoder[A]): DecodeResult[A] = da.decode(row)
   }
 
-  implicit class TraversableOps[A](val rows: Traversable[A])(implicit ea: RowEncoder[A]) {
+  implicit class TraversableOps[A](val rows: Traversable[A])(implicit ea: RowEncoder[A], engine: WriterEngine) {
     def asCsvString(sep: Char, header: Seq[String] = Seq.empty): String = {
       val out = new StringWriter()
       CsvWriter(out, sep, header).write(rows)
