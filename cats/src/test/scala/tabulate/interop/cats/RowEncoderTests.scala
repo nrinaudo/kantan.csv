7cf5775786d4d0c69c9c6fe74eb295ecb5a4d488
nrinaudo <nicolas@nrinaudo.com>
2015-12-30 16:06:17 +0100

    Add tests for contravariant functor instances.

---
+++ cats/src/test/scala/tabulate/interop/cats/RowEncoderTests.scala
@@ -0,0 +1,21 @@
+package tabulate.interop.cats
+
+import algebra.Eq
+import cats.laws.discipline.ContravariantTests
+import org.scalacheck.Arbitrary
+import org.scalatest.FunSuite
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.typelevel.discipline.scalatest.Discipline
+import tabulate.RowEncoder
+import tabulate.laws.discipline.arbitrary._
+import tabulate.laws.discipline.equality
+
+
+class RowEncoderTests extends FunSuite with GeneratorDrivenPropertyChecks with Discipline {
+  implicit def rowEncoderEq[A: Arbitrary]: Eq[RowEncoder[A]] = new Eq[RowEncoder[A]] {
+    override def eqv(a1: RowEncoder[A], a2: RowEncoder[A]): Boolean =
+      equality.rowEncoder(a1, a2)
+  }
+
+  checkAll("RowEncoder[Int]", ContravariantTests[RowEncoder].contravariant[Int, Int, Int])
+}
