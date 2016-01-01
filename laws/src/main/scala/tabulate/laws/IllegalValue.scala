bca5e4ae4eb4bea0c59875954886f073a4acdb06
nrinaudo <nicolas@nrinaudo.com>
2016-01-01 22:21:52 +0100

    Fix illegal char generator that generated valid chars.

--- laws/src/main/scala/tabulate/laws/IllegalValue.scala
+++ laws/src/main/scala/tabulate/laws/IllegalValue.scala
@@ -11,6 +11,10 @@ object IllegalValue {
   private def illegalNum[A]: Arbitrary[IllegalValue[A]] =
     Arbitrary(Gen.alphaChar.map(i => IllegalValue(i.toString)))
 
+  implicit val arChar: Arbitrary[IllegalValue[Char]] = Arbitrary(for {
+    h <- Arbitrary.arbitrary[Char]
+    t <- Gen.nonEmptyListOf(Arbitrary.arbitrary[Char])
+  } yield IllegalValue((h :: t).mkString))
   implicit val arbInt: Arbitrary[IllegalValue[Int]] = illegalNum[Int]
   implicit val arbFloat: Arbitrary[IllegalValue[Float]] = illegalNum[Float]
   implicit val arbDouble: Arbitrary[IllegalValue[Double]] = illegalNum[Double]
@@ -19,7 +23,6 @@ object IllegalValue {
   implicit val arbShort: Arbitrary[IllegalValue[Short]] = illegalNum[Short]
   implicit val arbBigInt: Arbitrary[IllegalValue[BigInt]] = illegalNum[BigInt]
   implicit val arbBigDecimal: Arbitrary[IllegalValue[BigDecimal]] = illegalNum[BigDecimal]
-  implicit val arbChar: Arbitrary[IllegalValue[Char]] = illegalNum[Char]
   implicit val arbUUID: Arbitrary[IllegalValue[UUID]] = illegalNum[UUID]
   implicit val arbBoolean: Arbitrary[IllegalValue[Boolean]] = Arbitrary(Arbitrary.arbitrary[Int].map(i => IllegalValue(i.toString)))
   implicit def arbOption[A](implicit arb: Arbitrary[IllegalValue[A]]): Arbitrary[IllegalValue[Option[A]]] =
