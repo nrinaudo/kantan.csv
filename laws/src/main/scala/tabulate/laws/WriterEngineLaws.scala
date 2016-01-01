10cdb8deaadca1d4c22c8b30c19a9ecbbb5c9784
nrinaudo <nicolas@nrinaudo.com>
2016-01-01 22:16:33 +0100

    Fix a test that fails when the last entry in a row is composed solely of whitespace.

--- laws/src/main/scala/tabulate/laws/WriterEngineLaws.scala
+++ laws/src/main/scala/tabulate/laws/WriterEngineLaws.scala
@@ -9,7 +9,8 @@ trait WriterEngineLaws {
   def roundTrip(csv: List[List[Cell]]): Boolean = csv.asCsv(',').unsafeReadCsv[List, List[Cell]](',', false) == csv
 
   def noTrailingSeparator(csv: List[List[Cell.NonEscaped]]): Boolean =
-    csv.asCsv(',').split("\n").map(_.trim).forall(!_.endsWith(","))
+    csv.asCsv(',').split("\n").forall(!_.endsWith(","))
+
 
   // This test is slightly dodgy, but works: we're assuming that the data is properly serialized (this is checked by
   // roundTrip), an want to make sure that we get the right number of rows. The `trim` bit is to allow for the optional
