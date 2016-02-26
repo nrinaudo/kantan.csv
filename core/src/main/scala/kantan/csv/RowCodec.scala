cf64fb924265adc6be3178d5884978a745a95868
nrinaudo <nicolas@nrinaudo.com>
2016-02-26 15:02:06 +0100

    Hide the combine method of RowCodec, as it encourages poor programming style.

--- core/src/main/scala/kantan/csv/RowCodec.scala
+++ core/src/main/scala/kantan/csv/RowCodec.scala
@@ -6,8 +6,6 @@ trait RowCodec[A] extends Codec[Seq[String], A, CsvError, RowDecoder, RowEncoder
 
 @export.exports(Subclass)
 object RowCodec extends GeneratedRowCodecs {
-  def combine[A](decoder: RowDecoder[A], encoder: RowEncoder[A]): RowCodec[A] = RowCodec(decoder.decode _, encoder.encode _)
-
   def apply[A](decoder: Seq[String] ⇒ CsvResult[A], encoder: A ⇒ Seq[String]): RowCodec[A] = new RowCodec[A] {
     override def encode(a: A) = encoder(a)
     override def decode(row: Seq[String]) = decoder(row)
