a0893f311d6d82a1adc10322c0dedb5df7cc4d3d
nrinaudo <nicolas@nrinaudo.com>
2016-03-08 14:29:56 +0100

    Remove unnecessary Xor, \/ and Maybe instances, inheritted from kantan.codecs.

--- cats/src/main/scala/kantan/csv/cats/package.scala
+++ cats/src/main/scala/kantan/csv/cats/package.scala
@@ -7,23 +7,11 @@ import kantan.csv
 
 /** Declares various type class instances for bridging `kantan.csv` and `cats`. */
 package object cats extends kantan.codecs.cats.CatsInstances {
-  implicit def xorCellDecoder[A, B](implicit da: CellDecoder[A], db: CellDecoder[B]): CellDecoder[Xor[A, B]] =
-      CellDecoder(s ⇒ da.decode(s).map(a ⇒ Xor.Left(a)).orElse(db.decode(s).map(b ⇒ Xor.Right(b))))
-
-  implicit def xorCellEncoder[A, B](implicit ea: CellEncoder[A], eb: CellEncoder[B]): CellEncoder[Xor[A, B]] =
-      CellEncoder(eab ⇒ eab match {
-        case Xor.Left(a)  ⇒ ea.encode(a)
-        case Xor.Right(b) ⇒ eb.encode(b)
-      })
-
   implicit def xorRowDecoder[A, B](implicit da: RowDecoder[A], db: RowDecoder[B]): RowDecoder[Xor[A, B]] =
       RowDecoder(row ⇒ da.decode(row).map(a ⇒ Xor.Left(a)).orElse(db.decode(row).map(b ⇒ Xor.Right(b))))
 
   implicit def xorRowEncoder[A, B](implicit ea: csv.RowEncoder[A], eb: csv.RowEncoder[B]): csv.RowEncoder[Xor[A, B]] =
-    csv.RowEncoder(xab ⇒ xab match {
-      case Xor.Left(a)  ⇒ ea.encode(a)
-      case Xor.Right(b) ⇒ eb.encode(b)
-    })
+    csv.RowEncoder(_.fold(ea.encode, eb.encode))
 
   implicit def foldableRowEncoder[A, F[_]](implicit ea: CellEncoder[A], F: Foldable[F]): RowEncoder[F[A]] =
     csv.RowEncoder(as ⇒ F.foldLeft(as, Seq.newBuilder[String])((acc, a) ⇒ acc += ea.encode(a)).result())
--- scalaz/src/main/scala/kantan/csv/scalaz/package.scala
+++ scalaz/src/main/scala/kantan/csv/scalaz/package.scala
@@ -1,50 +1,25 @@
 package kantan.csv
 
-import kantan.csv
-
 import _root_.scalaz.Maybe._
 import _root_.scalaz._, Scalaz._
 
 /** Declares various type class instances for bridging `kantan.csv` and `scalaz`. */
 package object scalaz extends kantan.codecs.scalaz.ScalazInstances {
-  implicit def eitherCellDecoder[A, B](implicit da: CellDecoder[A], db: CellDecoder[B]): CellDecoder[A \/ B] =
-    CellDecoder(s ⇒ da.decode(s).map(_.left[B]).orElse(db.decode(s).map(_.right[A])))
+  implicit def eitherRowDecoder[A, B](implicit da: RowDecoder[A], db: RowDecoder[B]): RowDecoder[A \/ B] =
+    RowDecoder(row ⇒ da.decode(row).map(_.left[B]).orElse(db.decode(row).map(_.right[A])))
 
-  implicit def maybeCellDecoder[A](implicit da: CellDecoder[A]): CellDecoder[Maybe[A]] = CellDecoder { s ⇒
-    if(s.isEmpty) DecodeResult.success(empty)
-    else          da.decode(s).map(just)
-  }
-
-  implicit def eitherCellEncoder[A, B](implicit ea: CellEncoder[A], eb: CellEncoder[B]): CellEncoder[A \/ B] =
-    new CellEncoder[\/[A, B]] {
-      override def encode(eab: \/[A, B]) = eab match {
-        case -\/(a)  ⇒ ea.encode(a)
-        case \/-(b)  ⇒ eb.encode(b)
-      }
-    }
-
-  implicit def maybeCellEncoder[A](implicit ea: CellEncoder[A]): CellEncoder[Maybe[A]] = new CellEncoder[Maybe[A]] {
-    override def encode(a: Maybe[A]) = a.map(ea.encode).getOrElse("")
-  }
-
-  implicit def eitherRowDecoder[A, B](implicit da: csv.RowDecoder[A], db: csv.RowDecoder[B]): csv.RowDecoder[A \/ B] =
-    csv.RowDecoder(row ⇒ da.decode(row).map(_.left[B]).orElse(db.decode(row).map(_.right[A])))
-
-  implicit def maybeRowDecoder[A](implicit da: csv.RowDecoder[A]): csv.RowDecoder[Maybe[A]] = csv.RowDecoder { row ⇒
+  implicit def maybeRowDecoder[A](implicit da: RowDecoder[A]): RowDecoder[Maybe[A]] = RowDecoder { row ⇒
     if(row.isEmpty) DecodeResult.success(empty)
     else            da.decode(row).map(just)
   }
 
-  implicit def eitherRowEncoder[A, B](implicit ea: csv.RowEncoder[A], eb: csv.RowEncoder[B]): csv.RowEncoder[A \/ B] =
-    csv.RowEncoder(eab ⇒ eab match {
-      case -\/(a)  ⇒ ea.encode(a)
-      case \/-(b)  ⇒ eb.encode(b)
-    })
+  implicit def eitherRowEncoder[A, B](implicit ea: RowEncoder[A], eb: RowEncoder[B]): RowEncoder[A \/ B] =
+    RowEncoder(_.fold(ea.encode, eb.encode))
 
-  implicit def foldableRowEncoder[A, F[_]](implicit ea: CellEncoder[A], F: Foldable[F]): csv.RowEncoder[F[A]] =
-    csv.RowEncoder(as ⇒ F.foldLeft(as, Seq.newBuilder[String])((acc, a) ⇒ acc += ea.encode(a)).result())
+  implicit def foldableRowEncoder[A, F[_]](implicit ea: CellEncoder[A], F: Foldable[F]): RowEncoder[F[A]] =
+    RowEncoder(as ⇒ F.foldLeft(as, Seq.newBuilder[String])((acc, a) ⇒ acc += ea.encode(a)).result())
 
-  implicit def maybeRowEncoder[A](implicit ea: csv.RowEncoder[A]): csv.RowEncoder[Maybe[A]] = new csv.RowEncoder[Maybe[A]] {
+  implicit def maybeRowEncoder[A](implicit ea: RowEncoder[A]): RowEncoder[Maybe[A]] = new RowEncoder[Maybe[A]] {
     override def encode(a: Maybe[A]) = a.map(ea.encode).getOrElse(Seq.empty)
   }
 
