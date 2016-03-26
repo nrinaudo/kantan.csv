9fdbb1eb490ee29fe68e03485505358c6b265840
nrinaudo <nicolas@nrinaudo.com>
2016-03-26 17:58:11 +0100

    Add documentation for CsvOutput.

--- core/src/main/scala/kantan/csv/CsvOutput.scala
+++ core/src/main/scala/kantan/csv/CsvOutput.scala
@@ -4,30 +4,76 @@ import java.io._
 import kantan.csv.engine.WriterEngine
 import scala.io.Codec
 
+/** Type class for all types that can be turned into [[CsvWriter]] instances.
+  *
+  * Instances of [[CsvOutput]] are rarely used directly. The preferred, idiomatic way is to use the implicit syntax
+  * provided by [[ops.CsvOutputOps CsvOutputOps]], brought in scope by importing `kantan.csv.ops._`.
+  *
+  * See the [[CsvOutput companion object]] for default implementations and construction methods.
+  */
 trait CsvOutput[-S] extends Serializable { self ⇒
+  /** Opens a `Writer` on the specified `S`. */
   def open(s: S): Writer
 
-  def writer[A: RowEncoder](s: S, separator: Char, header: Seq[String] = Seq.empty)(implicit engine: WriterEngine): CsvWriter[A] =
-    CsvWriter(open(s), separator, header)
+  /** Opens a [[CsvWriter]] on the specified `S`.
+    *
+    * @param s what to open a [[CsvWriter]] on.
+    * @param sep column separator.
+    * @param header optional header row, defaults to none.
+    */
+  def writer[A: RowEncoder](s: S, sep: Char, header: Seq[String] = Seq.empty)(implicit engine: WriterEngine): CsvWriter[A] =
+    CsvWriter(open(s), sep, header)
 
+  /** Writes the specified collections directly in the specifie `S`.
+    *
+    * @param s where to write the CSV data.
+    * @param rows CSV data to encode and serialise.
+    * @param sep column separator.
+    * @param header optional header row, defaults to none.
+    */
+  def write[A: RowEncoder](s: S, rows: TraversableOnce[A], sep: Char, header: Seq[String] = Seq.empty)(implicit engine: WriterEngine): Unit =
+    writer(s, sep, header).write(rows).close()
+
+  /** Turns a `CsvOutput[S]` into a `CsvOutput[T]`.
+    *
+    * This allows developers to adapt existing instances of [[CsvOutput]] rather than write one from scratch.
+    * One could, for example, write `CsvInput[File]` by basing it on `CsvInput[OutputStream]`:
+    * {{{
+    *   def fileOutput(implicit c: scala.io.Codec): CsvOutput[File] =
+    *     CsvOutput[OutputStream].contramap(f ⇒ new FileOutputStream(f, c.charSet))
+    * }}}
+    */
   def contramap[T](f: T ⇒ S): CsvOutput[T] = CsvOutput(t ⇒ self.open(f(t)))
-
-  def write[A: RowEncoder](out: S, rows: TraversableOnce[A], sep: Char, header: Seq[String] = Seq.empty)(implicit engine: WriterEngine): Unit =
-    writer(out, sep, header).write(rows).close()
 }
 
-trait LowPriorityCsvOutputs
-
-object CsvOutput extends LowPriorityCsvOutputs {
+/** Provides default instances as well as instance summoning and creation methods. */
+object CsvOutput {
+  /** Summons an implicit instance of `CsvOutput[A]` if one can be found.
+    *
+    * This is simply a convenience method. The two following calls are equivalent:
+    * {{{
+    *   val file: CsvOutput[File] = CsvOutput[File]
+    *   val file2: CsvOutput[File] = implicitly[CsvOutput[File]]
+    * }}}
+    */
   def apply[A](implicit oa: CsvOutput[A]): CsvOutput[A] = oa
 
+  /** Turns the specified function into a [[CsvOutput]].
+    *
+    * Note that it's usually better to compose an existing instance through [[CsvOutput.contramap]] rather than create
+    * one from scratch.
+    */
   def apply[A](f: A ⇒ Writer): CsvOutput[A] = new CsvOutput[A] {
     override def open(s: A): Writer = f(s)
   }
 
+  /** Default implementation for `Writer`. */
   implicit def writer: CsvOutput[Writer] = CsvOutput(w ⇒ w)
 
+  /** Default implementation for `OutputStream`. */
   implicit def outputStream(implicit codec: Codec): CsvOutput[OutputStream] =
     writer.contramap(o ⇒ new BufferedWriter(new OutputStreamWriter(o, codec.charSet)))
+
+  /** Default implementation for `File`. */
   implicit def file(implicit codec: Codec): CsvOutput[File] = outputStream.contramap(f ⇒ new FileOutputStream(f))
 }
--- docs/src/main/tut/15-csv_sinks.md
+++ docs/src/main/tut/15-csv_sinks.md
@@ -2,5 +2,50 @@
 layout: default
 title:  "What can CSV data be written to?"
 section: tutorial
-status: wip
 ---
+All the encoding tutorials, such as [this](13-serialising_collections.html) one, matter-of-factly call the
+[`asCsvWriter`] method of [`File`], when [`File`] does not in fact have such a method. This works thanks to the
+[`CsvOutput`] type class.
+
+What happens is, any type `A` such that there exists an implicit instance of `CsvOutput[A]` in scope will be enriched
+with various [useful methods]({{ site.baseurl }}/api/#kantan.csv.ops$$CsvOutputOps) for CSV serialisation. Various
+default implementations are automatically in scope, such as one for [`Writer`] or [`OutputStream`], but the most
+useful one is [`File`].
+
+## Implementation from scratch
+
+Reduced to its simplest expression, a [`CsvOutput`] is essentially a `A ⇒ Writer` - that is, a function that takes an 
+`A` and turns it into a [`Writer`] (note that this currently does not leave room for errors and implementations are
+forced to throw. This is something that will be fixed in later releases).
+
+If you can write such a function, you can trivially turn it into a valid instance of [`CsvOutput`] - for example,
+[`File`]:
+
+```tut:silent
+import kantan.csv._
+import java.io._
+import scala.io._
+
+implicit def fileOutput(implicit c: Codec): CsvOutput[File] =
+  CsvOutput(f ⇒ new OutputStreamWriter(new FileOutputStream(f), c.charSet)) 
+```
+
+## Adapting existing instances
+
+Most of the time though, it's easier to turn the type you wish to provide an instance for into a type that already has
+an instance - in our example, [`File`] can easily be turned into an [`OutputStream`], and [`OutputStream`] already has
+a [`CsvOutput`] instance. This is achieved through [`contramap`]:
+
+```tut:silent
+implicit def fileOutput(implicit c: Codec): CsvOutput[File] =
+  CsvOutput[OutputStream].contramap(f ⇒ new FileOutputStream(f)) 
+```
+
+
+[`asCsvWriter`]:{{ site.baseurl }}/api/#kantan.csv.ops$$CsvOutputOps@asCsvWriter[B](sep:Char,header:Seq[String])(implicitevidence$1:kantan.csv.RowEncoder[B],implicitoa:kantan.csv.CsvOutput[A],implicite:kantan.csv.engine.WriterEngine):kantan.csv.CsvWriter[B]
+[`CsvWriter`]:{{ site.baseurl }}/api/#kantan.csv.CsvWriter
+[`CsvOutput`]:{{ site.baseurl }}/api/#kantan.csv.CsvOutput
+[`Writer`]:https://docs.oracle.com/javase/7/docs/api/java/io/Writer.html
+[`File`]:https://docs.oracle.com/javase/7/docs/api/java/io/File.html
+[`OutputStream`]:https://docs.oracle.com/javase/7/docs/api/java/io/OutputStream.html
+[`contramap`]:{{ site.baseurl }}/api/#kantan.csv.CsvOutput@contramap[T](f:T=>S):kantan.csv.CsvOutput[T]
