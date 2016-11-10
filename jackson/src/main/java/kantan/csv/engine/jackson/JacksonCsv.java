b2b8d737a2b184afe0b14d9ce293a967f036698d
nrinaudo <nicolas@nrinaudo.com>
2016-11-10 10:57:04 +0100

    Appease unidoc by using complete class names rather than relying on imports to bring them in scope.

--- jackson/src/main/java/kantan/csv/engine/jackson/JacksonCsv.java
+++ jackson/src/main/java/kantan/csv/engine/jackson/JacksonCsv.java
@@ -18,11 +18,8 @@ package kantan.csv.engine.jackson;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.SequenceWriter;
-import com.fasterxml.jackson.dataformat.csv.CsvGenerator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvParser;
 
-import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 
@@ -31,17 +28,17 @@ public class JacksonCsv {
 
     static {
         MAPPER  = new CsvMapper();
-        MAPPER.enable(CsvParser.Feature.WRAP_AS_ARRAY);
-        MAPPER.enable(CsvGenerator.Feature.STRICT_CHECK_FOR_QUOTING);
+        MAPPER.enable(com.fasterxml.jackson.dataformat.csv.CsvParser.Feature.WRAP_AS_ARRAY);
+        MAPPER.enable(com.fasterxml.jackson.dataformat.csv.CsvGenerator.Feature.STRICT_CHECK_FOR_QUOTING);
     }
 
-    public static MappingIterator<String[]> parse(Reader reader, char separator) throws IOException {
+    public static MappingIterator<String[]> parse(Reader reader, char separator) throws java.io.IOException {
         return MAPPER.readerFor(String[].class)
                 .with(MAPPER.schemaFor(String[].class).withColumnSeparator(separator))
                 .readValues(reader);
     }
 
-    public static SequenceWriter write(Writer writer, char separator) throws IOException {
+    public static SequenceWriter write(Writer writer, char separator) throws java.io.IOException {
         return MAPPER.writer()
                 .with(MAPPER.schemaFor(String[].class).withColumnSeparator(separator).withLineSeparator("\r\n").withoutComments())
                 .writeValues(writer);
