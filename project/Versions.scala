3128fb9b3ba7c7a07fcb9605464bc6f1d647a0fc
nrinaudo <nicolas@nrinaudo.com>
2017-04-19 23:07:14 +0200

    Upgrade to 2.11.11 and 2.12.2.

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val commonsCsv        = "1.4"
   val jacksonCsv        = "2.8.7"
-  val kantanCodecs      = "0.1.12"
+  val kantanCodecs      = "0.2.0-SNAPSHOT"
   val opencsv           = "3.9"
   val productCollection = "1.4.5"
   val scalaCsv          = "1.3.4"
