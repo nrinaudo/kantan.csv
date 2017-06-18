62cd7bc120dc20a491bc2354d8e23c87c277084a
nrinaudo <nicolas@nrinaudo.com>
2017-06-18 17:58:28 +0200

    Update copyright date.

--- core/src/main/scala/kantan/csv/Codecs.scala
+++ core/src/main/scala/kantan/csv/Codecs.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Nicolas Rinaudo
+ * Copyright 2015 Nicolas Rinaudo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,3 +18,4 @@ package kantan.csv
 
 /** Defines default instances for [[CellEncoder]], [[CellDecoder]], [[RowEncoder]] and [[RowDecoder]]. */
 object codecs extends CellCodecInstances with RowCodecInstances with TupleInstances
+
