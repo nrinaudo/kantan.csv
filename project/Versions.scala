67fc038187d583fe1746c879e5072ae8e008e549
nrinaudo <nicolas@nrinaudo.com>
2017-07-03 16:22:23 +0200

    Upgrade dependencies.

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
   val commonsCsv        = "1.4"
-  val jacksonCsv        = "2.8.8"
+  val jacksonCsv        = "2.8.9"
   val kantanCodecs      = "0.2.1-SNAPSHOT"
-  val opencsv           = "3.9"
+  val opencsv           = "3.10"
   val productCollection = "1.4.5"
   val scalaCsv          = "1.3.4"
   val scalatest         = "3.0.3"
