865e13d981a41e64ec4f818cd3f8c138d2db5696
Nicolas Rinaudo <nicolas@nrinaudo.com>
2017-10-26 00:27:13 +0200

    Set version to 0.3.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val commonsCsv        = "1.5"
   val jacksonCsv        = "2.9.2"
-  val kantanCodecs      = "0.2.2-SNAPSHOT"
+  val kantanCodecs      = "0.2.2"
   val opencsv           = "4.0"
   val productCollection = "1.4.5"
   val scalaCsv          = "1.3.5"
