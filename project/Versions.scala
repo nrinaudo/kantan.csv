fe6ba8278934bb96506a0f94a891d1dbe372e13d
Nicolas Rinaudo <nicolas@nrinaudo.com>
2017-10-25 11:18:05 +0200

    Update to Scala 2.12.4 (and other dependencies)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
   val commonsCsv        = "1.5"
-  val jacksonCsv        = "2.9.0"
+  val jacksonCsv        = "2.9.2"
   val kantanCodecs      = "0.2.2-SNAPSHOT"
   val opencsv           = "4.0"
   val productCollection = "1.4.5"
   val scalaCsv          = "1.3.5"
   val scalatest         = "3.0.3"
-  val univocity         = "2.5.4"
+  val univocity         = "2.5.8"
 }
