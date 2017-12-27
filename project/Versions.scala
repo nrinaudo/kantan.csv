8020c27c6b864a5d8c479341ac201dc2058422e6
Nicolas Rinaudo <nicolas@nrinaudo.com>
2017-12-27 22:14:29 +0100

    Update to kantan.codecs 0.3.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val commonsCsv        = "1.5"
   val jacksonCsv        = "2.9.3"
-  val kantanCodecs      = "0.2.2"
+  val kantanCodecs      = "0.3.0"
   val opencsv           = "4.1"
   val productCollection = "1.4.5"
   val scalaCsv          = "1.3.5"
