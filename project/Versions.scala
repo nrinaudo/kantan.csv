2662eb48b0f55bfa217ba49132693933760e6e43
Scala Steward <me@scala-steward.org>
2020-05-17 02:28:35 +0200

    Update opencsv to 5.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val jacksonCsv       = "2.11.0"
   val jodaTime         = "2.10.6"
   val kantanCodecs     = "0.5.1"
-  val opencsv          = "5.1"
+  val opencsv          = "5.2"
   val scalaCsv         = "1.3.6"
   val univocity        = "2.8.4"
 }
