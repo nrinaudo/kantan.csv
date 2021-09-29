39431f27f47598e4ccd91e5eff05694767180773
Nicolas Rinaudo <nicolas@nrinaudo.com>
2021-09-29 20:14:11 +0200

    Update to scalacsv 1.3.8

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,6 +5,6 @@ object Versions {
   val jodaTime         = "2.10.8"
   val kantanCodecs     = "0.5.3"
   val opencsv          = "5.5.2"
-  val scalaCsv         = "1.3.6"
+  val scalaCsv         = "1.3.8"
   val univocity        = "2.9.1"
 }
