812608b4dfdd9647fc6503bc1cb50eef9b477843
Scala Steward <me@scala-steward.org>
2021-11-09 10:52:12 +0100

    Update scala-collection-compat to 2.6.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val commonsCsv       = "1.9.0"
-  val collectionCompat = "2.5.0"
+  val collectionCompat = "2.6.0"
   val jacksonCsv       = "2.13.1"
   val jodaTime         = "2.10.12"
   val kantanCodecs     = "0.5.3"
