137e619875ed6a954bfdbcc218fd5571ff22c29a
Scala Steward <me@scala-steward.org>
2022-03-20 20:10:25 +0100

    Update joda-time to 2.10.14

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val commonsCsv       = "1.9.0"
   val collectionCompat = "2.6.0"
   val jacksonCsv       = "2.13.1"
-  val jodaTime         = "2.10.13"
+  val jodaTime         = "2.10.14"
   val kantanCodecs     = "0.5.3"
   val opencsv          = "5.5.2"
   val scalaCsv         = "1.3.10"
