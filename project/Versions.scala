c30f0b64f21bcc0519040d882edf6a68ad7dc705
Scala Steward <scala_steward@virtuslab.com>
2024-06-24 20:31:07 +0000

    Update scala-csv to 1.4.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,6 +5,6 @@ object Versions {
   val jodaTime         = "2.10.13"
   val kantanCodecs     = "0.5.3"
   val opencsv          = "5.5.2"
-  val scalaCsv         = "1.3.10"
+  val scalaCsv         = "1.4.0"
   val univocity        = "2.9.1"
 }
