68177c62eb4e8affeec1b6dd7126e8120e764b26
Scala Steward <scala_steward@virtuslab.com>
2025-02-04 02:15:30 +0000

    Update scala-collection-compat to 2.13.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val commonsCsv       = "1.13.0"
-  val collectionCompat = "2.12.0"
+  val collectionCompat = "2.13.0"
   val jacksonCsv       = "2.18.1"
   val jodaTime         = "2.13.0"
   val kantanCodecs     = "0.6.0"
